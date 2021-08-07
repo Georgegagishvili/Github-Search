20317b3b2f72e2356decdc8a150bc6295986da3a
G1304 <georgegagishvili@gmail.com>
2021-08-07 16:13:03 +0400

    debounce

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,37 +1,51 @@
-import { useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { Link } from "react-router-dom";
-import styles from "./search_style.module.css";
-
-import Button from "@material-ui/core/Button";
 import { TextField } from "@material-ui/core";
+
+import _ from "lodash";
+import styles from "./search_style.module.css";
 
 export default function Search() {
   const [repoName, setRepoName] = useState("");
-  const [error, setError] = useState(null);
+  const [errror, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [repos, setRepos] = useState([]);
-  const RepSearch = (event) => {
-    event.preventDefault();
+
+  const sendQuery = (query) => {
     fetch(`https://api.github.com/repositories`)
       .then((res) => res.json())
       .then(
         (result) => {
           const filteredResult = result.filter((item) =>
-            item.name.includes(repoName)
+            item.name.includes(query)
           );
           setIsLoaded(true);
           setRepos(filteredResult);
         },
         (error) => {
           setError(error);
-          console.log(error);
+          console.log(errror, isLoaded);
         }
       );
   };
+
+  const delayedQuery = useCallback(
+    _.debounce((q) => sendQuery(q), 500),
+    []
+  );
+
+  useEffect(() => {
+    delayedQuery();
+  }, [delayedQuery]);
+  const onChange = (e) => {
+    setRepoName(e.target.value);
+    delayedQuery(e.target.value);
+  };
+
   return (
     <div>
       <form
-        onSubmit={RepSearch}
+        onSubmit={(e) => e.preventDefault()}
         className={styles.main_form}
         autoComplete="off"
       >
@@ -43,33 +57,26 @@ export default function Search() {
           name="search"
           id="search"
           value={repoName}
-          onChange={(un) => setRepoName(un.target.value)}
+          onChange={onChange}
         ></TextField>
-        <Button
-          type="submit"
-          variant="contained"
-          color="secondary"
-          size="large"
-        >
-          Search
-        </Button>
       </form>
       <div className={styles.repo_list}>
-        {repos.map((item) => {
-          return (
-            <div key={item.id} className={styles.single_repository}>
-              <Link
-                className={styles.single_repository_link}
-                to={{
-                  pathname: `/detailed/${item.owner.login}}/${item.name}`,
-                  state: item,
-                }}
-              >
-                {item.full_name}
-              </Link>
-            </div>
-          );
-        })}
+        {repoName &&
+          repos.map((item) => {
+            return (
+              <div key={item.id} className={styles.single_repository}>
+                <Link
+                  className={styles.single_repository_link}
+                  to={{
+                    pathname: `/detailed/${item.owner.login}}/${item.name}`,
+                    state: item,
+                  }}
+                >
+                  {item.full_name}
+                </Link>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
